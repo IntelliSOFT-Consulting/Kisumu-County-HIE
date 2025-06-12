3d73ea2510fe732dc57192421562cf5996078353
Brian Amolo <railamolo@gmail.com>
2025-06-12 15:06:54 +0300

    Support patient matching & client update functionality

--- start-hie.sh
+++ start-hie.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker network inspect hie >/dev/null 2>&1 || docker network create hie
 
 if [ -n "$1" ]; then
     docker compose -f hie/docker-compose.yml -f proxy/docker-compose.yml $@
